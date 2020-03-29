c5f3143f17376ecd9ac8ae39d8aac4f65b960aab
HimuraYuu <1016751002@qq.com>
2020-03-29 12:26:36 +0800

    Site updated: 2020-03-29 12:26:35

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -1,8 +1,8 @@
 // 评论issues仓库 by.removeif https://removeif.github.io/
-var repoIssuesUrl = "https://api.github.com/repos/removeif/blog_comment/issues";
+var repoIssuesUrl = "https://api.github.com/repos/HimuraYuu/blog_comments/issues";
 // 对应仓库 clientId、clientSecret 关于这两个参数的安全问题，查看 https://removeif.github.io/2019/09/19/博客源码分享.html#1-热门推荐，最新评论：
-var clientId = "46a9f3481b46ea0129d8";
-var clientSecret = "79c7c9cb847e141757d7864453bcbf89f0655b24";
+var clientId = "3b8c690ac915c928f640";
+var clientSecret = "1416ab1c07dae3536d48d49762e404fbc965de7c";
 var authorizationToken = 'Basic ' + btoa(clientId + ':' + clientSecret);
 // 写comment count值
 var reqCommentCountUrl = repoIssuesUrl + "?t=" + new Date().getTime() + "&labels=Gitalk,";
@@ -10,7 +10,7 @@ var reqCommentCountUrl = repoIssuesUrl + "?t=" + new Date().getTime() + "&labels
 var COMMENT_CACHE_KEY = "commentKey";
 
 // 管理员名称,评论时添加 [博主] 后缀
-var ADMIN_NAME = "removeif";
+var ADMIN_NAME = "HimuraYuu";
 
 function ajaxReqForGitHub(url, call) {
     $.ajax({
